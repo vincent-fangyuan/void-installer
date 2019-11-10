442af2b0742b3d59e6c83993c0b818780920b7b9
Joe Standring <git@joestandring.com>
2019-11-10 19:22:52 +0000

    Fix nm polkit change and directories
    Dotfiles should now clone into the correct location (/home/$USER/.dot) and therefore get moved to the home directory properly.
    Polkit code should be formatted correctly for NetworkManager

--- installer.sh
+++ installer.sh
@@ -15,12 +15,16 @@ xbps-install -Su
 xbps-install -S git neovim neofetch curl wget xorg-minimal calcurse setxkbmap dunst NetworkManager NetworkManager-openvpn network-manager-applet pywal feh pkg-config fontconfig-devel libX11-devel libXft-devel ncurses st-terminfo fish-shell compton cmus cmus-flac cmus-pulseaudio pulseaudio alsa-plugins-pulseaudio mpv newsboat ranger 
 
 echo -e "\e[31mCloning dotfiles...\e[0m"
-git clone https://github.com/joestandring/dotfiles ~/.dot
+git clone https://github.com/joestandring/dotfiles /home/$USER/.dot
 
 echo -e "\e[31mMoving files...\e[0m"
-cp .dot/.bashrc ~
-cp .dot/.xinitrc ~
-cp -r .dot/.config ~
+cp /home/$USER/.dot/.bashrc ~
+cp /home/$USER/.dot/.xinitrc ~
+cp -r /home/$USER/.dot/.config ~
 
 echo -e "\e[31mConfiguring Network Manager...\e[0m"
-echo 'polkit.addRule(function(action, subject) {\n  if (action.id.indexOf("org.freedesktop.NetworkManager.") == 0 &&\nsubject.isInGroup("network")) {\n    return polkit.Result.YES;\n  }\n});' > /etc/polkit-1/rules.d/50-org.freedesktop.NetworkManager.rules
+echo 'polkit.addRule(function(action, subject) {
+  if (action.id.indexOf("org.freedesktop.NetworkManager.") == 0 && subject.isInGroup("network")) {
+    return polkit.Result.YES;
+  }
+});' > /etc/polkit-1/rules.d/50-org.freedesktop.NetworkManager.rules
