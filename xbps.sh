ebde1818e3e7a5afc0e9b0094fa21a9f43258c88
Joe Standring <git@joestandring.com>
2019-11-10 19:24:35 +0000

    Create xbps.sh
    This file is temporary so I don't have to install all of the packages when loading a snapshot on VirtualBox

---
+++ xbps.sh
@@ -0,0 +1,2 @@
+# TEMP FILE REMOVE BEFORE FINAL RELEASE!
+xbps-install -S git neovim neofetch curl wget xorg-minimal calcurse setxkbmap dunst NetworkManager NetworkManager-openvpn network-manager-applet pywal feh pkg-config fontconfig-devel libX11-devel libXft-devel ncurses st-terminfo fish-shell compton cmus cmus-flac cmus-pulseaudio pulseaudio alsa-plugins-pulseaudio mpv newsboat ranger
