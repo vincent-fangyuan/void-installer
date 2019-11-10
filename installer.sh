adb391d7cdfa68f5fdcaad2105b15417ecc4df38
Joe Standring <git@joestandring.com>
2019-11-10 19:44:16 +0000

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -15,14 +15,13 @@ xbps-install -Su
 xbps-install -S git neovim neofetch curl wget xorg-minimal calcurse setxkbmap dunst NetworkManager NetworkManager-openvpn network-manager-applet pywal feh pkg-config fontconfig-devel libX11-devel libXft-devel ncurses st-terminfo fish-shell compton cmus cmus-flac cmus-pulseaudio pulseaudio alsa-plugins-pulseaudio mpv newsboat ranger 
 
 echo -e "\e[31mCloning dotfiles...\e[0m"
-cd ~
 git clone https://github.com/joestandring/dotfiles
 
-echo -e "\e[31mMoving files...\e[0m"
-cd dotfiles
-cp .bashrc ~
-cp .xinitrc ~
-cp .config ~
+#echo -e "\e[31mMoving files...\e[0m"
+#cd dotfiles
+#cp .bashrc ~
+#cp .xinitrc ~
+#cp .config ~
 
 echo -e "\e[31mConfiguring Network Manager...\e[0m"
 echo 'polkit.addRule(function(action, subject) {
