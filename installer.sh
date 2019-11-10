f0cd9a39381fd9073aa7815d7841dc7e831c8db7
Joe Standring <git@joestandring.com>
2019-11-10 20:03:52 +0000

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -5,25 +5,25 @@
 # GNU GPLv3
 
 echo -e "\e[31mChecking permissions...\e[0m"
-if [ "$EUID" -ne 0 ]; then
-	echo "Please run this script as root (e.g. sudo ./installer.sh)"
+if [ "$EUID" -eq 0 ]; then
+	echo "Please do not run this script as root (e.g. using sudo)"
 	exit
 fi
 
 echo -e "\e[31mUpdating and installing packages...\e[0m"
-xbps-install -Su
-xbps-install -S git neovim neofetch curl wget xorg-minimal calcurse setxkbmap dunst NetworkManager NetworkManager-openvpn network-manager-applet pywal feh pkg-config fontconfig-devel libX11-devel libXft-devel ncurses st-terminfo fish-shell compton cmus cmus-flac cmus-pulseaudio pulseaudio alsa-plugins-pulseaudio mpv newsboat ranger 
+sudo xbps-install -Su
+sudo xbps-install -S git neovim neofetch curl wget xorg-minimal calcurse setxkbmap dunst NetworkManager NetworkManager-openvpn network-manager-applet pywal feh pkg-config fontconfig-devel libX11-devel libXft-devel ncurses st-terminfo fish-shell compton cmus cmus-flac cmus-pulseaudio pulseaudio alsa-plugins-pulseaudio mpv newsboat ranger 
 
 echo -e "\e[31mCloning dotfiles...\e[0m"
 git clone https://github.com/joestandring/dotfiles
 
 echo -e "\e[31mMoving files...\e[0m"
-cp dotfiles/.bashrc /home/$USER
-cp dotfiles/.xinitrc /home/$USER
-cp -r dotfiles/.config /home/$USER
+cp dotfiles/.bashrc ~
+cp dotfiles/.xinitrc ~
+cp -r dotfiles/.config ~
 
 echo -e "\e[31mConfiguring Network Manager...\e[0m"
-echo 'polkit.addRule(function(action, subject) {
+sudo echo 'polkit.addRule(function(action, subject) {
   if (action.id.indexOf("org.freedesktop.NetworkManager.") == 0 && subject.isInGroup("network")) {
     return polkit.Result.YES;
   }
